3ab560924152eac74db27f28757f8353705d90db
antoniomarcucci <antonio.marcucci93@gmail.com>
2016-05-28 10:37:14 +0200

    ripristino

--- 2nd delivery/WEBSITE/script/script.js
+++ 2nd delivery/WEBSITE/script/script.js
@@ -1,31 +1,8 @@
-var GIndex=1;
-var GPos=0;
-var interval;
-
 $(document).ready(ready);
 
 function ready(){
-    "use strict";
 
     console.log("I'm ready!!");
-    
-    interval = setInterval(animateGallery, 2000);
-        
-    $("#img1").click(function(){
-        $("#mainColumn").css("background-image","url(../img/1.png)");
-        clearInterval(interval);
-        
-    });
-    
-    $("#img2").click(function(){
-        $("#mainColumn").css("background-image","url(../img/2.png)");
-        clearInterval(interval);
-    });
-    
-    $("#img3").click(function(){
-        $("#mainColumn").css("background-image","url(../img/3.png)");
-        clearInterval(interval);
-    });
     
     var id=1;
     
@@ -47,16 +24,4 @@ function ready(){
             console.log("Error");
         }
     });
-}
-
-function animateGallery() {
-    if(window.GIndex==3){
-        window.GIndex=1;
-    }else{
-        window.GIndex++;
-    }
-    console.log(window.GIndex);
-
-    $("#mainColumn").css("background-image","url(../img/"+window.GIndex+".png)");
-    $("#slideButton").css("margin-top",3+((window.GIndex-1)*133));
 }
