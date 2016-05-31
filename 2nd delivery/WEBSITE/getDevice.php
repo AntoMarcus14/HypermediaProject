fd8996659ddc9a58a51c3162d15d81999aacd9cb
VitoMatarazzo <vitomata20@gmail.com>
2016-05-31 18:01:53 +0200

    added script post

--- 2nd delivery/WEBSITE/getDevice.php
+++ 2nd delivery/WEBSITE/getDevice.php
@@ -4,34 +4,39 @@
 //echo "I'm the php";
 
 //connection to db
-$mysqli = new mysqli("localhost", "root", "", "tim_hyp_db");
+$mysqli = mysqli_connect("localhost", "root", "", "tim_hyp_db");
 
+//OR die
 if (mysqli_connect_errno()) { //verify connection
     echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
     exit(); //do nothing else 
 }
 else {
-    //echo "Successful connection"; // connection ok
+    //echo "Successful connection\n"; // connection ok
 
     # extract results mysqli_result::fetch_array
-    $query = " SELECT * FROM device  ";
+    $query = " SELECT * FROM device ";
     //query execution
-    $result = $mysqli->query($query);
+    $result = mysqli_query($mysqli, $query);
     //if there are data available
-    if($result->num_rows >0)
+    if($numRows=mysqli_num_rows($result) >0)
     {
+        //echo $numRows;
         $myArray = array();//create an array
-        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+        foreach(mysqli_fetch_array($result, MYSQLI_ASSOC) as $row) {
+            
             $myArray[] = $row;
         }
+    
+        //var_dump($myArray);
         echo json_encode($myArray);
     }
 
     //free result
-    $result->close();
+    mysqli_free_result($result);
 
     //close connection
-    $mysqli->close();
+    mysqli_close($mysqli);
 
 
 
--- 2nd delivery/WEBSITE/script/scriptPost.js
+++ 2nd delivery/WEBSITE/script/scriptPost.js
@@ -7,16 +7,19 @@ function ready(){
         //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
         url: "../getDevice.php", //Relative or absolute path to file.php file
-        data: {device:id},
+        //data: {device:id},
         success: function(response) {
             console.log(JSON.parse(response));
-            var devices=JSON.parse(response);    
-            var el+="<div class='course' id='c'><h2>"devices[0].Name"</h2><span>"+devices[0].Description+"</span></div>";             
+            var devices=JSON.parse(response);               
             
-            $("body").html(el);
+            $(".device-name").html(devices[0]);
+            $(".description").html(devices[2]);
+            $(".color").attr("src",devices[8]);
+            
         },
         error: function(request,error) 
         {
             console.log("Error");
         }
     });
+}
