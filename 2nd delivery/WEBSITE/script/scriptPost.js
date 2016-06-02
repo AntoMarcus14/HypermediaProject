0c80b3d85f488a539e8bcc71456f9eb196dbfb1f
VitoMatarazzo <vitomata20@gmail.com>
2016-06-02 19:01:20 +0200

    device images added

--- 2nd delivery/WEBSITE/script/scriptPost.js
+++ 2nd delivery/WEBSITE/script/scriptPost.js
@@ -14,12 +14,37 @@ function ready(){
             
             $(".device-name").html(devices[0]);
             $(".description").html(devices[2]);
+            $(".fullPrice").html(devices[3]);
+            $(".monthPrice").html(devices[4]);
             $(".color").attr("src",devices[8]);
+            $("#mainDeviceImg").attr("src",devices[5]);
+            $("#1devicePic").attr("src",devices[5]);
+            $("#2devicePic").attr("src",devices[6]);
+            $("#3devicePic").attr("src",devices[7]);
             
         },
         error: function(request,error) 
         {
             console.log("Error");
         }
+    });
+    
+    
+    $("#1devicePic").click(function(){
+        $("#mainDeviceImg").fadeOut( "fast", function(){
+            $("#mainDeviceImg").attr("src",$("#1devicePic").attr("src")).fadeIn();
+        });
+    });
+    
+    $("#2devicePic").click(function(){
+        $("#mainDeviceImg").fadeOut( "fast", function(){
+            $("#mainDeviceImg").attr("src",$("#2devicePic").attr("src")).fadeIn();
+        });
+    });
+    
+    $("#3devicePic").click(function(){
+        $("#mainDeviceImg").fadeOut( "fast", function(){
+            $("#mainDeviceImg").attr("src",$("#3devicePic").attr("src")).fadeIn();
+        });
     });
 }
