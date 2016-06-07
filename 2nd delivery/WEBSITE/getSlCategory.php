1f729cc2a44f72f6ef69cd4c9f98f1e5fd55d2f2
sergiomorenzetti <sergio.morenzetti@mail.polimi.it>
2016-06-07 14:40:30 +0200

    php for category

---
+++ 2nd delivery/WEBSITE/getSlCategory.php
@@ -0,0 +1,44 @@
+<?php
+//get all the course from db and reply using json structure
+
+//echo "I'm the php";
+
+//connection to db
+$mysqli = mysqli_connect("localhost", "root", "", "my_tim3m");
+
+//OR die
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else 
+}
+else {
+    //echo "Successful connection\n"; // connection ok
+
+    # extract results mysqli_result::fetch_array
+    $query = " SELECT * FROM device LEFT JOIN TechCharacteristics ON device.Name = TechCharacteristics.Device WHERE device.Name ='Samsung Gear Fit' ";
+    //query execution
+    $result = mysqli_query($mysqli, $query);
+    //if there are data available
+    if(mysqli_num_rows($result) >0)
+    {
+        //echo mysqli_num_rows($result);
+        $myArray = array();//create an array
+        while($row = mysqli_fetch_assoc($result)) {
+            
+            //echo $row["Category"];
+            $myArray[] = $row;
+            //$i++;
+            //echo $row[0];
+        }
+    
+        //var_dump($myArray);
+        echo json_encode($myArray);
+    }
+
+    //free result
+    mysqli_free_result($result);
+
+    //close connection
+    mysqli_close($mysqli);
+}
+?>
