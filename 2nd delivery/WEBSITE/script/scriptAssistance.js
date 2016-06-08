e92a3bf9852a819a69f411def086fef92a41d29a
VitoMatarazzo <vitomata20@gmail.com>
2016-06-08 20:48:41 +0200

    dynamic assistance service page

--- 2nd delivery/WEBSITE/script/scriptAssistance.js
+++ 2nd delivery/WEBSITE/script/scriptAssistance.js
@@ -1,5 +1,25 @@
 $(document).ready(ready);
 
+var firstPanel="<div class=\"panel panel-default\">\
+                        <div class=\"panel-heading\">\
+                            <h4 class=\"panel-title\">\
+                                <a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse1\"><span class=\"glyphicon glyphicon-minus-sign faqIcon\"></span></a>\
+                            </h4>\
+                        </div>\
+                        <div id=\"collapse1\" class=\"panel-collapse collapse in\">\
+                            <div class=\"panel-body\"></div>\
+                        </div>\
+                </div>";
+var nextPanel="<div class=\"panel panel-default\">\
+                        <div class=\"panel-heading\">\
+                            <h4 class=\"panel-title\">\
+                                <a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse1\"><span class=\"glyphicon glyphicon-plus-sign faqIcon\"></span></a>\
+                            </h4>\
+                        </div>\
+                        <div id=\"collapse1\" class=\"panel-collapse collapse\">\
+                            <div class=\"panel-body\"></div>\
+                        </div>\
+                </div>";
 
 function ready(){
     
@@ -17,30 +37,41 @@ function ready(){
             $(".asDescription").html(aService[0].Description);
             $(".asImage").attr("src", aService[0].Image);
             
-            $(".slList").html(devices[0].SLServices);
-            var tab="";
-            var i=0;
-            for(var index in devices[0]) {
-                if (devices[0].hasOwnProperty(index)&&i>=11&&devices[0][index]!=null) {
-                    tab=tab+"<tr><td>" + index + "</td><td>" + devices[0][index] + "</td></tr> ";
+            if(aService[0].hasOwnProperty('Question')){
+                var faqContent=firstPanel;
+                for(i=1; i<aService.length; i++) {
+                    faqContent=faqContent + nextPanel;
                 }
-                i++;
+                $("#accordion").html(faqContent);
+                $(".panel-default").each(function(i,element){
+                        $(element).find("a").attr("href","#collapse" + i);
+                        $(element).find("a").prepend(aService[i].Question);
+                        $(element).find(".panel-collapse").attr("id","collapse" + i);
+                        $(element).find(".panel-body").html(aService[i].Answer.replace(/euro/g,"€"));
+                });
             }
-            $("#tech" + ">Table").html(tab);
-            
+            else{
+                $("#faq").hide();
+            }
         },
         error: function(request,error) 
         {
             console.log("Error");
         }
     });  
-      
 
+  /*$(".panel-title").on(click, function(){
+      $(this).find("span").attr("class","glyphicon glyphicon-plus-sign faqIcon");
+  }):
+  $(".panel-title").on(click, function(){
+      $(this).find("span").attr("class","glyphicon glyphicon-plus-sign faqIcon");
+  }):*/
 
-  $(".collapse").on("hide.bs.collapse", function(){
+  $(".panel-collapse").on("hide.bs.collapse", function(){
+      console.log("Hiding");
       $(this).parent().find("span").attr("class","glyphicon glyphicon-plus-sign faqIcon");
   });
-  $(".collapse").on("show.bs.collapse", function(){
+  $(".panel-collapse").on("show.bs.collapse", function(){
       $(this).parent().find("span").attr("class","glyphicon glyphicon-minus-sign faqIcon");
-  });
+  }); 
 }
