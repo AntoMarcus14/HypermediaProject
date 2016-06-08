a4f88cfc92789fb408e40b5ecd009dc17a20782d
VitoMatarazzo <vitomata20@gmail.com>
2016-06-08 19:10:34 +0200

    assistance service page

--- 2nd delivery/WEBSITE/script/scriptAssistance.js
+++ 2nd delivery/WEBSITE/script/scriptAssistance.js
@@ -2,12 +2,45 @@ $(document).ready(ready);
 
 
 function ready(){
+    
+  $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "../getAS.php", //Relative or absolute path to file.php file
+        //data: {device:id},
+        success: function(response) {
+            console.log(JSON.parse(response));
+            var aService=JSON.parse(response);               
+            
+            $(".title-with-tab").html(aService[0].Name);
+            $(".asDescription").html(aService[0].Description);
+            $(".asImage").attr("src", aService[0].Image);
+            
+            $(".slList").html(devices[0].SLServices);
+            var tab="";
+            var i=0;
+            for(var index in devices[0]) {
+                if (devices[0].hasOwnProperty(index)&&i>=11&&devices[0][index]!=null) {
+                    tab=tab+"<tr><td>" + index + "</td><td>" + devices[0][index] + "</td></tr> ";
+                }
+                i++;
+            }
+            $("#tech" + ">Table").html(tab);
+            
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });  
+      
+
 
   $(".collapse").on("hide.bs.collapse", function(){
       $(this).parent().find("span").attr("class","glyphicon glyphicon-plus-sign faqIcon");
   });
   $(".collapse").on("show.bs.collapse", function(){
-    
       $(this).parent().find("span").attr("class","glyphicon glyphicon-minus-sign faqIcon");
   });
 }
