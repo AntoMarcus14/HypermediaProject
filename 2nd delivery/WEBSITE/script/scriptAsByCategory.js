d9f0e460a71506e52d77c76f2e9b5e956ec9ba7f
sergiomorenzetti <sergio.morenzetti@mail.polimi.it>
2016-06-12 20:24:20 +0200

    AsByCat almost finished

--- 2nd delivery/WEBSITE/script/scriptAsByCategory.js
+++ 2nd delivery/WEBSITE/script/scriptAsByCategory.js
@@ -2,33 +2,33 @@ $(document).ready(ready);
 
 var noVerticalDiv = "<div class=\"col-sm-6\">\
                             <h3 class=\"assistance-category-subtitle\"></h3>\
-                            <a><u><h4 id=\"name-of-assistance\"></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
                         </div>";
 
 var verticalDiv="<div class=\"col-sm-6 verticalLine\">\
                             <h3 class=\"assistance-category-subtitle\"></h3>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
                         </div>";
 
-var verticalDiv1="<div class=\"col-sm-12\">\
+var noVerticalDiv1="<div class=\"col-sm-12\">\
                             <h3 class=\"assistance-category-subtitle\"></h3>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
                         </div>";
 
-var verticalDiv2="<div class=\"col-sm-12\">\
+var noVerticalDiv2="<div class=\"col-sm-12 assistance-by-format\">\
                             <h3 class=\"assistance-category-subtitle\"></h3>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
-                            <a><u><h4 id=\"name-of-assistance\></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
+                            <a><u><h4 class=\"name-of-assistance\"></h4></u></a>\
                         </div>";
 
 var hrDiv = "<hr id=\"SL-category-hr\">";
@@ -58,8 +58,14 @@ function ready(){
                     bol=1;
                 }
             }
-            if(bol==0){
-                content=content+"<div class=\"row assistance-image-background\">";
+            if(bol==1){
+                content=content+"<div class=\"row\">\
+                <div class=\"col-sm-2\">\
+                    <div class=\"row assistance-by-format\">\
+                    <h3 id=\"filter-panel-title\">Filter Panel</h3>\
+                    </div>\
+                </div>\
+                <div class=\"col-sm-10 assistance-by-format\">";
                 //premessa: la query deve ritornare un elenco di row che siano ordinate in base alle subcategory, e non distribuite casualmente
                 //in modo che quando vengono contate le diverse subcategory non ci siano ripetizioni
                 var count=1;
@@ -71,42 +77,50 @@ function ready(){
                 }
                 if(count%2==0){
                     for(i=0;i<count;i=i+2){
+                        content = content + "<div class=\"row\">";
                         content=content+noVerticalDiv+verticalDiv;
+                        content = content + "</div>";
                         if(i+2!=count){
                             content=content+hrDiv;
                         }
                     }
                 } else {
-                    for(i=0;i<count;i++){
-                        if(i%2==0){
-                            content=content+noVerticalDiv;
-                        } else if(i+1==count){
-                            content=content+noVerticalDiv1
+                    i=0;
+                    while(i<count){
+                        content = content + "<div class=\"row\">";
+                        if(count-i==1){
+                            content = content + noVerticalDiv1;
+                            content = content + "</div>";
+                            i++;
                         } else {
-                            content=content+verticalDiv+hrDiv;
+                            content= content + noVerticalDiv + verticalDiv;
+                            content = content + "</div>" + hrDiv;
+                            i=i+2;
                         }
                     }
                 }
+                content = content + "</div>";
             } else {
-                content=content+"<div class=\"row\">\
-                <h1 class=\"title-without-tab\">Assistance by category</h1>\
-            </div>\
-            <div class=\"row\">\
-                <div class=\"col-sm-2\">\
-                    <div class=\"row assistance-by-format\">\
-                    </div>\
-                </div>\
-                <div class=\"col-sm-10 assistance-image-background\">";
-                content=content+verticalDiv2;               
+                content=content+"<div class=\"row assistance-image-background\">";
+                content=content+noVerticalDiv2;
             }
             content=content+"</div>";
             
             $(".background-category").append(content);
-            
+            /*
+            subcat=assistances[0].SubCategory;
+            for(i=1; i<assistances.length; i++){
+                if(subcat!=assistances[i].SubCategory){
+                    assistances[i-1].SubCategory=assistances[i].SubCategory;
+                    subcat = assistances[i].SubCategory;
+                }
+            }
+            */
             $(".assistance-category-subtitle").each(function(i,element){
                 $(element).html(assistances[i].SubCategory);
             });
-            $("#name-of-assistance").each(function(i,element){
+            
+            $(".name-of-assistance").each(function(i,element){
                 $(element).html(assistances[i].Name);
             });
         },
