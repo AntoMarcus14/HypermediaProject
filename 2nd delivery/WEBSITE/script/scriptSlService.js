d47d3028bcd28b745b4f95193da796fc94d8ef63
antoniomarcucci <antonio.marcucci93@gmail.com>
2016-06-15 16:13:29 +0200

    Starting dynamic sl service

---
+++ 2nd delivery/WEBSITE/script/scriptSlService.js
@@ -0,0 +1,60 @@
+$(document).ready(ready);
+
+function ready(){
+    var param = (window.location.search.replace("?","")).split("=");
+    var catName = decodeURIComponent(param[1]);
+    if(param.length>=2){
+    $(".title-without-tab").html(catName);
+    $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "../getSlService.php", //Relative or absolute path to file.php file
+        data : {type : catName},
+        success: function(response) {
+            console.log(JSON.parse(response));
+            var category=JSON.parse(response);  //è un array di array associativo (primo indice: numero riga, secondo indice: nome attributo)      
+            var content="";
+            var i=0;//quanti div di contenuto ho messo
+            var j=0;//quanti div di contenuto restano da mettere in una riga
+            //ALGORITMO:controllare per ogni riga se ce ne sono 2, se sono meno..., se sono piu valutare se ne ho almeno altri 2--> metto 3 in //una riga
+            //e poi rinizio a fare la stessa valutazione sulla nuova riga
+            while(i < category.length){
+                content=content+"<div class=\"row\">";
+                j = category.length - i;
+                if(j==1){
+                    content = content + noVerticalDiv1 + "</div>";
+                    i++;
+                } else if(j==2 || j==4){
+                    content = content + noVerticalDiv1 + verticalDiv1 + "</div>";
+                    i = i+2;
+                    if(j==4){
+                        content = content + hrDiv;
+                    }
+                } else if(j==3 || j>4){
+                    content = content + noVerticalDiv2 + verticalDiv2 + verticalDiv2 + "</div>";
+                    i = i+3;
+                    if(j>4){
+                        content = content + hrDiv;                        
+                    }
+                }
+            }
+            $(".background-category").append(content);
+            $(".category-subtitle").each(function(i,element){
+                $(element).html(category[i].Title);
+            })
+            $(".slDesc").each(function(i,element){
+                $(element).html(category[i].Description);
+            })
+            $(".category-images").each(function(i,element){
+                $(element).attr("src", category[i].Image);
+            })
+            
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });
+    }
+}
