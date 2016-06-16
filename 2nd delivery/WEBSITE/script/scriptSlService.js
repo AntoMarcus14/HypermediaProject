897c0414c7bb904acb2259cbb1caa7d66560f5ed
antoniomarcucci <antonio.marcucci93@gmail.com>
2016-06-16 17:41:58 +0200

    dynamic sl finished

--- 2nd delivery/WEBSITE/script/scriptSlService.js
+++ 2nd delivery/WEBSITE/script/scriptSlService.js
@@ -14,18 +14,28 @@ function ready(){
         success: function(response) {
             console.log(JSON.parse(response));
             var category=JSON.parse(response);  //è un array di array associativo (primo indice: numero riga, secondo indice: nome attributo)      
-            
+            if(category[0].SmartLife=="WellUp"||category[0].SmartLife=="TIM Wallet"){
+                $("#subscript").hide();
+            }
             $(".descriptSl").html(category[0].Description);
             $("#rightImgSL").attr("src",category[0].Image);
             $(".subtitle").html(category[0].Subtitle);
             $(".content").html(category[0].Content);
             $(".glyphicon-arrow-up").parent().append("Up to Smart Life Category");
             $("#title").html(category[0].SmartLife);
-            $("#subtit").html(category[0].FirstSubtitle);
+            if(category[0].SmartLife=="WellUp"){
+               $("#subtit").hide();
+               $(".videoTag2").attr("src",category[0].SecondDescr);
+               $("#desc2").hide();  
+            }else{
+              $("#subtit").html(category[0].FirstSubtitle);
+              $(".videoTag2").hide();  
+              $("#desc2").html(category[0].SecondDescr);
+            }
             $("#desc").html(category[0].FirstDescription);
             $("#imageSl").attr("src",category[0].FirstImage);
             $("#subtit2").html(category[0].SecondSubtitle);
-            $("#desc2").html(category[0].SecondDescr);
+            $("#summary").append(category[0].Subscription);
         },
         error: function(request,error) 
         {
